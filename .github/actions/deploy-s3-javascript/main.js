9047c1a68e13c36a2830f75e2532e23a8cebfed6
davidkvoacweb <davidkovacwebdev@gmail.com>
2023-12-18 14:23:26 +0100

    added first custom js aciton

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,9 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const exec = require("@actions/core");
+
+function run() {
+    core.notice('Hello mothafocka')
+}
+
+run();
